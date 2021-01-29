895ed45d92c0af1b0b8bb4507c9326825eac734b
Carine Batista <carinedcb@hotmail.com>
2021-01-28 22:32:45 -0300

    Add Scripts.js

---
+++ Projeto/scripts.js
@@ -0,0 +1,77 @@
+const Modal = {
+	open(){
+		//Abrir Modal
+		//Adicionar a class active ao modal
+		document
+			.querySelector('.modal-overlay')
+			.classList
+			.add('active')
+	},
+	close(){
+		//Fechar o modal
+		//remover a class active do modal
+		document
+			.querySelector('.modal-overlay')
+			.classList
+			.remove('active')
+	}
+}
+
+const transactions =[
+	{
+		id:1,
+		description: 'Luz',
+		amount: -50000,
+		date: '23/01/2021',
+	},
+	{
+		id:2,
+		description: 'Website',
+		amount: 500000,
+		date: '23/01/2021',
+	},
+	{
+		id:3,
+		description: 'Internet',
+		amount: -20000,
+		date: '23/01/2021',
+	}
+]
+
+const Transaction = {
+	incomes(){
+		// somar as entradas
+	},
+	expenses(){
+		// somar as saídas
+	},
+	total(){
+		// entradas- saídas
+	}
+}
+
+const DOM = {
+	addTransaction(transaction, index){
+		const tr= document.createElement('tr')
+		tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+		
+	},
+	innerHTMLTransaction(transaction) {
+
+
+		const html = `
+			<td class="description">${transaction.description}</td>
+			<td class="expense">${transaction.amount}</td>
+			<td class="date">${transaction.date}</td>
+			<td>
+				<img src="./assets/minus.svg" alt="Remover Transação">
+			</td>
+		`
+ 
+		return html
+	}
+}
+
+
+DOM.addTransaction(transactions[1])
