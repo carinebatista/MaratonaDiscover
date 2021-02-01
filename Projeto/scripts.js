11d24e23bacfd873895214fa7a9b2370deb20ce7
Carine Batista <carinedcb@hotmail.com>
2021-02-01 19:22:34 -0300

    Add Remove to Transaction

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -19,19 +19,16 @@ const Modal = {
 
 const transactions =[
 	{
-		id:1,
 		description: 'Luz',
 		amount: -50000,
 		date: '23/01/2021',
 	},
 	{
-		id:2,
 		description: 'Website',
 		amount: 500000,
 		date: '23/01/2021',
 	},
 	{
-		id:3,
 		description: 'Internet',
 		amount: -20000,
 		date: '23/01/2021',
@@ -43,6 +40,13 @@ const Transaction = {
 
 	add(transaction){
 		Transaction.all.push(transaction)
+
+		App.reload()
+	},
+
+	remove(index){
+		Transaction.all.splice(index, 1)
+
 		App.reload()
 	},
 
@@ -156,10 +160,3 @@ const App ={
 }
 
 App.init()
-
-Transaction.add({
-	id: 39,
-	description: 'Alo',
-	amount:200,
-	date: '23/01/2021'
-})
